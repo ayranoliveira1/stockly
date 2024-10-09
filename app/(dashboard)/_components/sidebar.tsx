2ac73c9c50b64a9decea5831703c4ad852a2b977
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-10-09 14:26:17 -0300

    feat: addd font inter

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -2,7 +2,7 @@ const SideBar = () => {
    return (
       <div className="w-72 bg-white">
          <div className="px-8 py-6">
-            <h1 className="font-bold text-2xl">STOCLY</h1>
+            <h1 className="text-2xl font-bold">STOCLY</h1>
          </div>
 
          <div className="flex flex-col gap-2 p-2">
