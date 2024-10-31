6a76a5c1fdf5f150e5d1f3b1d58008781e172390
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-10-30 21:34:49 -0300

    feat: ensure product stock is restored after sale is deleted

--- app/_actions/sales/delete-sales/index.ts
+++ app/_actions/sales/delete-sales/index.ts
@@ -7,10 +7,36 @@ import { revalidatePath } from "next/cache";
 export const deleteSale = async ({ id }: DeleteSaleType) => {
    deleteSaleSchema.parse({ id });
 
-   await db.sale.delete({
-      where: {
-         id,
-      },
+   await db.$transaction(async (trx) => {
+      const sale = await trx.sale.findUnique({
+         where: {
+            id,
+         },
+         include: {
+            salesProducts: true,
+         },
+      });
+
+      if (!sale) return;
+
+      await trx.sale.delete({
+         where: {
+            id,
+         },
+      });
+
+      for (const product of sale.salesProducts) {
+         await trx.product.update({
+            where: {
+               id: product.productId,
+            },
+            data: {
+               stock: {
+                  increment: product.quantity,
+               },
+            },
+         });
+      }
    });
 
    revalidatePath("/sales");
