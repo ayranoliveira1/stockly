97099e2cb7aeb2b0283289ef610c79ecb4644763
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-11-20 22:52:05 -0300

    feat: add commit function

--- app/_helpers/currency.ts
+++ app/_helpers/currency.ts
@@ -1,3 +1,9 @@
+/**
+ * Formats a given number as a currency string in Brazilian Real (BRL).
+ *
+ * @param value - The numeric value to be formatted as currency.
+ * @returns A string representing the formatted currency in "pt-BR" locale.
+ */
 export const formatCurrency = (value: number) => {
    return new Intl.NumberFormat("pt-BR", {
       style: "currency",
